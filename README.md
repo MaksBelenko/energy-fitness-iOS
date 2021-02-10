c2c6652d85b8674f6caef9e48b048ce6f9940bad
MaksBelenko <maksim.belenko@gmail.com>
2021-02-10 11:14:30 +0000

    Added readme

---
+++ README.md
@@ -0,0 +1,15 @@
+<p align="center">
+  <img src="https://raw.githubusercontent.com/energygym/energygym.github.io/master/images/circle-ENERGY.png" width="150" alt="Nest Logo" />
+</p>
+  
+  <p align="center"> <b>Energy Fitness</b>
+
+  <p align="center">iOS application for Energy Fitness repository</p>
+
+
+<a href="https://app.bitrise.io/app/8342aab647c5aaef/status.svg?token=Iyn-yx5a98w_exzZPGvZgQ"><img src="https://app.bitrise.io/app/8342aab647c5aaef/status.svg?token=Iyn-yx5a98w_exzZPGvZgQ" alt="Bitrise CI" /></a>
+
+  <!--[![Backers on Open Collective](https://opencollective.com/nest/backers/badge.svg)](https://opencollective.com/nest#backer)
+  [![Sponsors on Open Collective](https://opencollective.com/nest/sponsors/badge.svg)](https://opencollective.com/nest#sponsor)-->
+
+## Description
--- UnitTests/WeekCalendar/Models/MonthEnumExtTests.swift
+++ UnitTests/WeekCalendar/Models/MonthEnumExtTests.swift
@@ -31,41 +31,63 @@ class MonthEnumExtTests: XCTestCase {
         XCTAssert(month.getLocalisedName(format: .ThreeLetters) == "Jan")
     }
     
-    func test_getNumber() throws {
+    func test_getNumber_January() throws {
         month = .January
-        XCTAssert(month.getNumber() == 1, "Should be equal to 1")
-        
+        XCTAssert(month.getNumber() == 1, "Should be equal to 1 but is equal to \(month.getNumber())")
+    }
+    
+    func test_getNumber_February() throws {
         month = .February
-        XCTAssert(month.getNumber() == 2, "Should be equal to 2")
-        
+        XCTAssert(month.getNumber() == 2, "Should be equal to 2 but is equal to \(month.getNumber())")
+    }
+    
+    func test_getNumber_March() throws {
         month = .March
-        XCTAssert(month.getNumber() == 3, "Should be equal to 3")
-        
+        XCTAssert(month.getNumber() == 3, "Should be equal to 3 but is equal to \(month.getNumber())")
+    }
+    
+    func test_getNumber_April() throws {
         month = .April
-        XCTAssert(month.getNumber() == 4, "Should be equal to 4")
-        
+        XCTAssert(month.getNumber() == 4, "Should be equal to 4 but is equal to \(month.getNumber())")
+    }
+    
+    func test_getNumber_May() throws {
         month = .May
-        XCTAssert(month.getNumber() == 5, "Should be equal to 5")
-        
+        XCTAssert(month.getNumber() == 5, "Should be equal to 5 but is equal to \(month.getNumber())")
+    }
+    
+    func test_getNumber_June() throws {
         month = .June
-        XCTAssert(month.getNumber() == 6, "Should be equal to 6")
-        
+        XCTAssert(month.getNumber() == 6, "Should be equal to 6 but is equal to \(month.getNumber())")
+    }
+    
+    func test_getNumber_July() throws {
         month = .July
-        XCTAssert(month.getNumber() == 7, "Should be equal to 7")
-        
+        XCTAssert(month.getNumber() == 7, "Should be equal to 7 but is equal to \(month.getNumber())")
+    }
+    
+    func test_getNumber_August() throws {
         month = .August
-        XCTAssert(month.getNumber() == 8, "Should be equal to 8")
-        
+        XCTAssert(month.getNumber() == 8, "Should be equal to 8 but is equal to \(month.getNumber())")
+    }
+    
+    func test_getNumber_September() throws {
         month = .September
-        XCTAssert(month.getNumber() == 9, "Should be equal to 9")
-        
+        XCTAssert(month.getNumber() == 9, "Should be equal to 9 but is equal to \(month.getNumber())")
+    }
+    
+    func test_getNumber_October() throws {
         month = .October
-        XCTAssert(month.getNumber() == 10, "Should be equal to 10")
-        
+        XCTAssert(month.getNumber() == 10, "Should be equal to 10 but is equal to \(month.getNumber())")
+    }
+    
+    func test_getNumber_November() throws {
         month = .November
-        XCTAssert(month.getNumber() == 11, "Should be equal to 11")
-        
+        XCTAssert(month.getNumber() == 11, "Should be equal to 11 but is equal to \(month.getNumber())")
+    }
+    
+    func test_getNumber_December() throws {
         month = .December
-        XCTAssert(month.getNumber() == 12, "Should be equal to 12")
+        XCTAssert(month.getNumber() == 12, "Should be equal to 12 but is equal to \(month.getNumber())")
     }
 }
